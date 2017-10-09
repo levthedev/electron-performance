ed5a6e149d0ca92882b0cac9ca3cfe5083230f16
Lev Kravinsky <kravinskylev@gmail.com>
2017-10-09 15:45:28 -0400

    sample electron scrolling performance

---
+++ main.js
@@ -0,0 +1,34 @@
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+const path = require('path')
+const url = require('url')
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 1600,
+    height: 1200,
+    webPreferences: {
+      nodeIntegration: false
+    }
+  })
+  mainWindow.loadURL('https://chat.zulip.org')
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
